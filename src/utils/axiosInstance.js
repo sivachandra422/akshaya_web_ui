a1ad8a3c2d63d5c87bc4eb9cb960906da442bba8
SivaChandra422 <siva.sivachandra23@gmail.com>
2025-05-24 10:45:28 +0530

    API calls data fetch fixing

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,10 +1,10 @@
 
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://akshaya-backend-service.onrender.com',
+  baseURL: "https://akshaya-backend-service.onrender.com", // Corrected: removed "/api"
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
